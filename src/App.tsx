0804720a41384fc9e1ba5f79c8ec7bc43696f05c
zhenann-htp <zhenann.htp@gmail.com>
2022-04-17 21:48:24 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '@elastic/eui/dist/eui_theme_light.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { EuiProvider } from "@elastic/eui";
+import { CustomForm } from "./components/customForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <EuiProvider>
+        <CustomForm />
+      </EuiProvider>
     </div>
   );
 }
---
+++ src/components/customForm.tsx
@@ -0,0 +1,135 @@
+import React, { FunctionComponent, useState } from "react";
+import {
+  EuiPage,
+  EuiPageHeader,
+  EuiButton,
+  EuiPageContentBody,
+  EuiPageBody,
+  EuiFlexGrid,
+  EuiFlexItem,
+  EuiPanel,
+  EuiFieldText,
+  EuiFormRow,
+  EuiText,
+  EuiFlexGroup,
+} from "@elastic/eui";
+
+class Inp {
+  type: string = "text";
+  id: number = 1;
+  value: string = "";
+}
+
+export const CustomForm = ({ button = <></> }) => {
+  const [value, setValue] = useState("");
+
+  const [arr, setArr] = useState([new Inp()]);
+
+  const addInput = () => {
+    setArr((s) => {
+      return [...s, new Inp()];
+    });
+  };
+
+  const handleChange = (e: any) => {
+    e.preventDefault();
+
+    const index = e.target.id;
+    setArr((s) => {
+      const newArr = s.slice();
+      if (newArr[index] !== undefined) newArr[index].value = e.target.value;
+
+      return newArr;
+    });
+  };
+
+  const forJS = (e: String) => {
+    return e;
+  };
+
+  const forAjax = (e: String) => {
+    return `$data['${e}'] = $param['${e}'];`;
+  };
+
+  const forAPI = (e: String) => {
+    return `$${e} = !empty($param['data']['${e}'])?trim($param['data']['${e}']): '';`;
+  };
+
+  return (
+    <EuiPage paddingSize="l">
+      <EuiPageBody>
+        <EuiPageHeader
+          iconType="logoElastic"
+          pageTitle="Page title"
+          rightSideItems={[button, <EuiButton>Do something</EuiButton>]}
+          bottomBorder
+        />
+        <EuiPageContentBody>
+          <EuiFlexGrid columns={1}>
+            <EuiFlexItem>
+              <EuiPanel >
+                <EuiFlexItem>
+                  {arr.map((item, i) => {
+                    item.id = i;
+                    return (
+                      <EuiFormRow id={item.id.toString()} key={i}>
+                        <EuiFieldText
+                          placeholder="Placeholder text"
+                          value={item.value}
+                          onChange={handleChange}
+                          key={i}
+                          id={item.id.toString()}
+                        />
+                      </EuiFormRow>
+                    );
+                  })}
+                </EuiFlexItem>
+                <EuiFlexGroup
+                  responsive={false}
+                  wrap
+                  gutterSize="s"
+                  alignItems="center"
+                >
+                  <EuiFlexItem grow={false}>
+                    <EuiButton onClick={addInput} iconType="heart">
+                      Primary
+                    </EuiButton>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiPanel>
+            </EuiFlexItem>
+          </EuiFlexGrid>
+          <EuiFlexGrid columns={3}>
+            <EuiFlexItem>
+              <EuiPanel >
+                {arr.map((item, i) => {
+                  if (item.value) return <EuiText>{forJS(item.value)}</EuiText>;
+                })}
+              </EuiPanel>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiPanel >
+                <EuiText>
+                  {arr.map((item, i) => {
+                    if (item.value)
+                      return <EuiText>{forAjax(item.value)}</EuiText>;
+                  })}
+                </EuiText>
+              </EuiPanel>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiPanel >
+                <EuiText>
+                  {arr.map((item, i) => {
+                    if (item.value)
+                      return <EuiText>{forAPI(item.value)}</EuiText>;
+                  })}
+                </EuiText>
+              </EuiPanel>
+            </EuiFlexItem>
+          </EuiFlexGrid>
+        </EuiPageContentBody>
+      </EuiPageBody>
+    </EuiPage>
+  );
+};
